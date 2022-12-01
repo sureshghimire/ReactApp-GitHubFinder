af1dfb2b52fceda0a8b2392af8aa03d0a1c64f9b
sureshghimire <61361644+sureshghimire@users.noreply.github.com>
2022-12-01 15:36:51 -0600

    Update Footer.jsx
    Footer position changed

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   const footerYear = new Date().getFullYear()
 
   return (
-    <footer className='footer p-10 bg-gray-700 text-primary-content footer-center'>
+    <footer className='footer p-10 bg-gray-700 text-primary-content footer-center position-fixed'>
       <div>
         <svg
           width='50'
